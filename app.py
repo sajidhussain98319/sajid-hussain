e58cba0d85b93dfb7a5ffe53d82083b0fff878b1
sajidhussain98319 <120622932+sajidhussain98319@users.noreply.github.com>
2025-03-14 02:46:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from test import test
-app = test(__name__)
+from flask import flask
+app = flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! This is Sajid Test Program 2025'
+    return 'Hello World! This is Sajid flask Program 2025'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
