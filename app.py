720b154852d90737cb2d6dd5c0aa86930a8b0d3a
sajidhussain98319 <120622932+sajidhussain98319@users.noreply.github.com>
2025-03-19 23:48:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-from flask import flask
-app = flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-<<<<<<< HEAD
-    return 'Hello World! This is networking'
-=======
-    return 'Hello World! This is Sajid networking'
->>>>>>> 6e7dfe6921bac6d85e07af8c207f3b15643a2a8d
+    return 'Hello World! This is sajid Test Program 22'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
